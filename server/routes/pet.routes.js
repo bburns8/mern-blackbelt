093161f8505782df3446cb525f47c9863b73f5c2
bburns858@gmail.com <blake@Blakes-MacBook-Pro.local>
2020-11-16 15:26:32 -0800

    inital commit

---
+++ server/routes/pet.routes.js
@@ -0,0 +1,10 @@
+const PetController = require('../controllers/pet.controller');
+
+module.exports = function(app){
+    app.post('/api/pet', PetController.createPet);
+    app.get('/api/pets', PetController.getAll);
+    app.get('/api/pet/:_id', PetController.getOne);
+    app.put('/api/pet/:_id', PetController.updateObject);
+    app.delete('/api/pet/:_id', PetController.deleteObject);
+    app.put('/api/like/:_id', PetController.likePet);
+}
